410d9f5a471fab5bab1e1979d642baef01ebd383
Aron Pereira <50774150+pereiraaron@users.noreply.github.com>
2026-03-09 18:58:54 +0530

    Fixed tailwind issues

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Aron",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName ?? "Guest"}
+            subtext="Access and manage your account and transactions effectively"
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
